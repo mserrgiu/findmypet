334a1b969e35affaf0a4859c0efb9286890f1306
mserrgiu <mserrgiu@gmail.com>
2024-07-13 23:24:10 +0400

    test

--- lib/findmypet.dart
+++ lib/findmypet.dart
@@ -6,11 +6,8 @@ class FindMyPet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Text('First Text Here')],
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
